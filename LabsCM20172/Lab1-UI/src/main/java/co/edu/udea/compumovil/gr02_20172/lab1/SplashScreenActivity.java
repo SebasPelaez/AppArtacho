e2016e96e6919624ec901563540c023cf44db9ec
Juan Sebastián Peláez <sebaspvilla@hotmail.com>
2017-08-18 07:27:10 -0500

    Se crea el proyecto, Splash Activity y Loggin Activity

---
+++ LabsCM20172/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package co.edu.udea.compumovil.gr02_20172.lab1;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        SplashScreenActivity.this, Loggin.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
